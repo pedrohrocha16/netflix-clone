d991b63e245d9b1a222e56fb9cda811ac5a6cacd
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-06-02 15:01:37 -0300

    corrigindo responsividade

--- src/components/layout/Plano.jsx
+++ src/components/layout/Plano.jsx
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom'
 function Plano() {
   return (
    <div className={styles.plano}>
-         <img src={popCorn} alt="popcorn" className={styles.popcorn}/>
          <div className={styles.desc}>
+            <img src={popCorn} alt="popcorn" className={styles.popcorn}/>
             <h3>A Netflix que você adora por apenas R$18,90.</h3>
             <p className={styles.desc_p}>Assine o plano Padrão com anúncios.</p>
             <Link patch="/">Saiba Mais &#62;</Link>
